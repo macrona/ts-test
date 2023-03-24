fe3db04e0717ea30d720d912c4ffebcfc17692fb
mac <mac@macrona-2.local>
2023-03-25 02:58:21 +0800

    feat: 路由

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,34 @@
-import React from 'react';
-import './App.css';
-import Tab from './components/tab/Tab';
+import React from "react";
+// import { Routes, Route, Redi } from "react-router";
+import { Navigate, RouteObject, useRoutes } from "react-router-dom";
+import {isLogined} from './utils/auth'
+import "./App.css";
+import Login from "./pages/Login";
+import Main from "./pages/Main";
+import Profile from "./pages/Profile";
 
 function App() {
-  return (
-    <div className="App">
+  
+  const routes = useRoutes([
+    {
+      path: "/",
+      element: isLogined() ? <Main/> : <Navigate to='/login' />,
+      children: [
+        {
+          path: "/profile",
+          element: <Profile />,
+        },
+      ]
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
     
-    </div>
+  ]);
+
+  return (
+    <div>{routes}</div>
   );
 }
 
